e27f59f20e0d1c50edf45320738d1d2c93be5db4
肖昕 <xiao.xin@outlook.com>
2025-07-20 12:06:18 +0800

    add sealed type test

---
+++ src/SourceGeneration.ChangeTracking.Test/TrackObject/SealedTypeTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SourceGeneration.ChangeTracking;
+
+[TestClass]
+public class SealedTypeTest
+{
+    [TestMethod]
+    public void Test()
+    {
+        SealedType tracking = new();
+
+        ((ICascadingChangeTracking)tracking).AcceptChanges();
+
+        Assert.IsFalse(((ICascadingChangeTracking)tracking).IsChanged);
+        Assert.IsFalse(((ICascadingChangeTracking)tracking).IsCascadingChanged);
+
+        tracking.A = 1;
+        Assert.IsTrue(((ICascadingChangeTracking)tracking).IsChanged);
+        Assert.IsFalse(((ICascadingChangeTracking)tracking).IsCascadingChanged);
+
+    }
+}
+
+[ChangeTracking]
+public sealed partial class SealedType
+{
+    public partial int A { get; set; }
+}
